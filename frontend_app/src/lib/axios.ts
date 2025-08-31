d9a83e5531818923dcedd0e6bafa4a89a750e069
Gedeon-Luzolo <gedeluz9@gmail.com>
2025-08-31 17:31:39 +0100

    feat : Ajout des dimensions responsives 2

--- frontend_app/src/lib/axios.ts
+++ frontend_app/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const url = "http://localhost:4000";
-// export const url = "";
+// export const url = "http://localhost:4000";
+export const url = "";
 
 export const api = (contentType: string = "application/json") => {
   return axios.create({
